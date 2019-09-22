87b685c46ff87d15270a74c7380cf1238e648568
Luram Archanjo <luram.archanjo@sensedia.com>
2019-09-22 18:22:47 -0300

    Interpolation

---
+++ src/main/java/com/example/Interpolation.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class Interpolation {
+
+  public static void main(String[] args) {
+    String hmtl = """
+                <html>
+                  <head>
+                    <title>This web page was generated using java interpolation</title>
+                  </head>
+                  <body>
+                    <h1>This it is work, welcome Java 13</h1>
+                  </body>
+                </html>
+                """;
+    System.out.println(hmtl);
+  }
+
+}
